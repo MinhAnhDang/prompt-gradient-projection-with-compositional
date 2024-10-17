33c7e9a79d182de4f66dc51babd0b4181deb3ea0
minhanh.dang <dangminhanh64@gmail.com>
2024-10-17 09:54:25 +0700

    Increase #epoch after each task

--- l2p-pgp/engine.py
+++ l2p-pgp/engine.py
@@ -412,3 +412,5 @@ def train_and_evaluate(model: torch.nn.Module, model_without_ddp: torch.nn.Modul
         if args.output_dir and utils.is_main_process():
             with open(os.path.join(args.output_dir, '{}_stats.txt'.format(datetime.datetime.now().strftime('log_%Y_%m_%d_%H_%M'))), 'a') as f:
                 f.write(json.dumps(log_stats) + '\n')
+                
+        args.epoch = args.epoch * 2
